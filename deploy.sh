31cbdaa9fe0702b4a2476928dbcdf2cae9ac4329
Mike Yuan <me@yhndnzj.com>
2019-05-25 17:38:45 +0800

    Configure travis-ci

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 git config --global user.name "Mike Yuan" 
 git config --global user.email "me@yhndnzj.com"
+sed -i "s/GH_TOKEN/$GH_TOKEN" _config.yml
 git clone https://github.com/YHNdnzj/yhndnzj.github.io.git -b master .deploy_git
 hexo deploy
