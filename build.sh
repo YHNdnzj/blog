783764dc94f55b6ef962a5f5f66605f9871af699
Mike Yuan <me@yhndnzj.com>
2019-05-25 17:44:41 +0800

    Configure travis-ci

--- build.sh
+++ build.sh
@@ -2,6 +2,5 @@
 cd themes/suka
 yarn install --production
 cd $TRAVIS_BUILD_DIR
-sed -i "s/GH_TOKEN/$GH_TOKEN" _config.yml
 hexo clean
 hexo generate
--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@
 git config --global user.name "Mike Yuan" 
 git config --global user.email "me@yhndnzj.com"
 git clone https://github.com/YHNdnzj/yhndnzj.github.io.git -b master .deploy_git
+sed -i "s/GH_TOKEN/$GH_TOKEN/" _config.yml
 hexo deploy
