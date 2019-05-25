df3078c31e42e16956830de4ed192bfe113a6f24
Mike Yuan <me@yhndnzj.com>
2019-05-25 14:53:51 +0800

    Fix typo

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 git config --global user.name "Mike Yuan" 
 git config --global user.email "me@yhndnzj.com"
-git clone https://github.com/YHNdnzj/yhndnzj.github.io.git .deploy_git
+git clone https://github.com/YHNdnzj/yhndnzj.github.io.git -b master .deploy_git
 hexo deploy
