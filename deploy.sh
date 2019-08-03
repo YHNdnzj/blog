b63fb79a8626956225a9220f807249df1709789b
Mike Yuan <me@yhndnzj.com>
2019-08-03 11:04:45 +0800

    Use /bin/bash

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 git config --global user.name "Mike Yuan" 
 git config --global user.email "me@yhndnzj.com"
 git clone https://github.com/YHNdnzj/yhndnzj.github.io.git -b master .deploy_git
--- install-theme.sh
+++ install-theme.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 cd themes/suka
 yarn install --production
