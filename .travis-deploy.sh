4ec56e5502ecd16128505d157aa4dcda6c2ccc3b
Mike Yuan <me@yhndnzj.com>
2019-09-15 00:51:50 +0800

    Revert "Use gh-pages branch"
    This reverts commit 432fbbc059dd040624dddf394e1ea43d481ed509.

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git config --global user.name "Mike Yuan" 
 git config --global user.email "me@yhndnzj.com"
-git clone https://github.com/YHNdnzj/yhndnzj.github.io.git -b gh-pages .deploy_git
+git clone https://github.com/YHNdnzj/yhndnzj.github.io.git -b master .deploy_git
 sed -i "s/GH_TOKEN/$GH_TOKEN/" _config.yml
 hexo deploy
