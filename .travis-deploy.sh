65a3af22354f3025541b17c115cfa8690350cee5
Mike Yuan <me@yhndnzj.com>
2019-10-12 21:08:11 +0800

    Update .travis-deploy.sh

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -3,4 +3,4 @@ git config --global user.name "Mike Yuan"
 git config --global user.email "me@yhndnzj.com"
 git clone https://github.com/YHNdnzj/yhndnzj.github.io.git -b master .deploy_git
 sed -i "s/GH_TOKEN/$GH_TOKEN/" _config.yml
-hexo deploy
+yarn run deploy
