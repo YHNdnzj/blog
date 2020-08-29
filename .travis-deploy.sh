42f5c2fc790853332b86cc46a05dcb7ed23cf7cd
Mike Yuan <me@yhndnzj.com>
2020-08-29 22:52:50 +0800

    Force push to master branch

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 git config --global user.name "Mike Yuan" 
 git config --global user.email "me@yhndnzj.com"
-git clone https://github.com/YHNdnzj/yhndnzj.github.io.git -b master .deploy_git
 sed -i "s/GH_TOKEN/$GH_TOKEN/" _config.yml
 yarn run deploy
