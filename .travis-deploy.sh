e66081bbf1a0d7ffe01cc15f35f23dada1ead67a
Mike Yuan <me@yhndnzj.com>
2021-09-25 14:46:16 +0800

    Remove travis-ci related files

--- .travis-deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git config --global user.name "Mike Yuan" 
-git config --global user.email "me@yhndnzj.com"
-sed -i "s/GH_TOKEN/$GH_TOKEN/" _config.yml
-yarn run deploy
--- .travis-install-theme.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd themes/suka
-yarn install --production
